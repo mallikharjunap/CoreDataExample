bbe7ab855ce7cf660a5d798785484a6ccf44f270
mallikharjunaGupta <mallikharjunaGuptaP@ecssi.com>
2024-02-13 16:00:09 +0530

    Update and Edit and Delete the item from list

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -51,6 +51,40 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item =  models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel,handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default,handler: { _ in
+            
+            let alert =  UIAlertController(title: "Edit Item", message: "Edit Your Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text =  item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel,handler: { [weak self] _ in
+                
+                guard let field = alert.textFields?.first,let newName = field.text ,!newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+                
+                
+            }))
+            
+            self.present(alert, animated: true)
+            
+            
+            
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive,handler: { [weak self]_ in
+            self?.deleteItem(item: item)
+        }))
+        present(sheet, animated: true)
+    }
     
     
     
@@ -105,6 +139,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         do{
             try contex.save()
+            getAllitem()
         }catch{
             
         }
@@ -116,6 +151,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         item.name = newName
         do{
             try contex.save()
+            getAllitem()
         }catch{
             
         }
